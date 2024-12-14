f153f40c1d9574a40ee268a389440ee65a400ace
Tarık Direk <tarkdirek@gmail.com>
2024-12-14 09:55:12 +0300

    Error handling configuration done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// Initialize and center the map
+
+
 var map = L.map("map").setView([37.878563, 41.125728], 13);
 
-// Add a tile layer (e.g., OpenStreetMap)
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -16,7 +16,6 @@ let button = document.getElementsByClassName("search-btn")[0];
 button.addEventListener("click", async () => {
   let inputValue = document.getElementsByClassName("tracker-ip")[0];
   let data = await getLocation(inputValue.value);
-
   parseData(data);
   let lat = data.location.lat;
   let long = data.location.lng;
@@ -30,18 +29,39 @@ button.addEventListener("click", async () => {
 });
 
 async function getLocation(value) {
-  let response = await fetch(
-    `https://geo.ipify.org/api/v2/country,city?apiKey=at_JMS6uGnytQJvAOZitn9SHE7ct2bB2&domain=${value}`
-  );
-  let data = response.json();
-  return data;
+  try {
+    const ipRegex =
+      /^(25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)$/;
+    const isIp = ipRegex.test(value); // Check if input is an IP address
+    const parameter = isIp ? `ipAddress=${value}` : `domain=${value}`;
+
+    let response = await fetch(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=at_JMS6uGnytQJvAOZitn9SHE7ct2bB2&${parameter}`
+    );
+
+    if (!response.ok) {
+      if (response.status === 422) {
+        alert("IP address or domain was not found. Please try another one.");
+      } else {
+        alert(`An error occurred: ${response.statusText}`);
+      }
+      return null;
+    }
+
+    let data = await response.json();
+    return data;
+  } catch (error) {
+    alert("An unexpected error occurred. Please try again.");
+    console.error(error);
+    return null;
+  }
 }
 
 function parseData(data) {
   let paragraphs = document.querySelectorAll(".data-parse");
   paragraphs[0].innerText = data.ip;
   paragraphs[1].innerText =
-    data.location.city + ", " + data.location.region+ data.location.postalCode;
+    data.location.city + ", " + data.location.region + data.location.postalCode;
   paragraphs[2].innerText = `UTC${data.location.timezone}`;
   paragraphs[3].innerText = data.isp ? data.isp : "None";
 }
